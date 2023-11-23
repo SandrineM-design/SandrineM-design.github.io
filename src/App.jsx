d75eb5a4e6fa97ba93daed4b435f424aeab9a5ff
SandrineM-design <mansonsandrine@gmail.com>
2023-11-23 16:46:02 +0100

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import './App.css'
-import NavBar from "./components/Navbar";
+import "./App.css";
+import { Link } from "react-router-dom";
 import Presentation from "./components/Presentation";
 import Education from "./components/Education";
 import Experiences from "./components/Experiences";
-import Hardskills from "./components/Hardskils";
-import Softskills from "./components/Softskills";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Hobbies from "./components/Hobbies";
 
 function App() {
-   return (
+  return (
     <>
       <nav>
-        <NavBar />
+        <Link to="/">Presentation</Link>
+        <Link to="/curiculum">Curiculum Vitae</Link>
+        <Link to="/projects">Projets</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
       <main>
         <Presentation>Sandrine MANSON</Presentation>
         <Education>Formation</Education>
         <Experiences>Expériences professionnelles</Experiences>
-        <Hardskills>Compétences professionnelles</Hardskills>
-        <Softskills>Softskills</Softskills>
+        <Skills>Compétences professionnelles</Skills>
         <Projects>Mes projets professionnels</Projects>
         <Hobbies>Mes hobbies</Hobbies>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Presentation from "./components/Presentation.jsx";
+import Curiculum from "./pages/Curiculum.jsx";
+import Projects from "./components/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Presentation</Link>
+          <Link to="/curiculum">Curiculum Vitae</Link>
+          <Link to="/projects">Projets</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <main>
+          <Presentation />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/curiculum",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Presentation</Link>
+          <Link to="/curiculum">Curiculum Vitae</Link>
+          <Link to="/projects">Projets</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <main>
+          <Curiculum />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/projects",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Presentation</Link>
+          <Link to="/curiculum">Curiculum Vitae</Link>
+          <Link to="/projects">Projets</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <main>
+          <Projects />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/contact",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Presentation</Link>
+          <Link to="/curiculum">Curiculum Vitae</Link>
+          <Link to="/projects">Projets</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <main>
+          <Contact />
+        </main>
+      </>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
