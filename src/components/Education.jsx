23c7feed863076585b557f0f64eeaf94fd2293ce
SandrineM-design <mansonsandrine@gmail.com>
2023-12-01 16:22:46 +0100

    css navbar

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 function Education(props) {
+  const formations = [
+    {
+      name: "Bootcamp Développeur Web et Web Mobile",
+      school: "Wild Code School",
+      date: "du 11 septembre 2023 au 9 février 2024",
+    },
+    {
+      name: "HTML5/CSS3, Git-GitHub, Algorithmes, API Rest, Javascript",
+      school: "OpenClassrooms, cours en ligne",
+      date: "depuis mars 2023",
+    },
+    {
+      name: "Baccalauréat Scientifique mention AB",
+      school: "Lycée Guez de Balzac, Anougoulème (16)",
+      date: "juin 2005",
+    },
+  ];
   return (
     <div>
       <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Mes formations</h2>
-      <ul>
-        <li>
-          Bootcamp Développeur Web et Web Mobile <br />
-          Wild Code School, Campus Remote <br />
-          Du 11 septembre 2023 au 9 février 2024
-        </li>
-        <li>
-          OpenClassrooms, Cours en ligne <br />
-          HTML5/CSS3, Git/GitHub, Algorithmes, API Rest <br />A partir de mars
-          2023
-        </li>
-        <li>
-          Baccalauréat scientifique Mention AB <br />
-          Lycée Guez de Balzac, Angoulème (16) <br />
-          Juin 2005
-        </li>
-      </ul>
     </div>
   );
 }
