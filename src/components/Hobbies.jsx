ad3816df854d88d758c12e840d21963514e48949
SandrineM-design <mansonsandrine@gmail.com>
2023-12-13 17:31:31 +0100

    complete hobbies component

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Hobbies(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      Sur mon temps libre, je suis Sapeure Pompier Volontaire au centre de
+      secours de <br />
+      ma commune de résidence. Mes missions principales sont le secours aux
+      personnes, la mise en sécurité <br />
+      des biens et des personnes dans les lieux privés ou publics. <br />
+      J'aime les balades dans la nature, le bricolage, le jardinage, ... en
+      famille. Je suis également une grande lectrice. Mes styles favoris sont
+      les romans policiers, la science <br />
+      fiction et les romans historiques.
+    </div>
+  );
 }
 
 export default Hobbies;
