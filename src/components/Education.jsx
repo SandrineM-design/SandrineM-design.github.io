b3ee0382c19bd1662c0ff3d88f74984b48562469
SandrineM-design <mansonsandrine@gmail.com>
2023-12-13 17:27:06 +0100

    complete Education component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
 function Education(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>Formations</h1>
+      <ul>
+        <li>
+          Bootcamp Développeur Web et Web Mobile <br />
+          Wild Code School, Campus Remote <br />
+          Du 11 septembre 2023 au 9 février 2024
+        </li>
+        <li>
+          OpenClassrooms, Cours en ligne <br />
+          HTML5/CSS3, Git/GitHub, Algorithmes, API Rest <br />A partir de mars
+          2023
+        </li>
+        <li>
+          Baccalauréat scientifique Mention AB <br />
+          Lycée Guez de Balzac, Angoulème (16) <br />
+          Juin 2005
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Education;
