7e48924c290b4bad81d042f75d3b468ddc47af87
SandrineM-design <mansonsandrine@gmail.com>
2024-01-10 16:18:03 +0100

    update curiculum component

--- src/pages/Curiculum.jsx
+++ src/pages/Curiculum.jsx
@@ -4,10 +4,7 @@ import photo2 from "../assets/Juracine.jpg";
 function Curiculum() {
   return (
     <div className="flex flex-col flex-1 place-items-center bg-yellow-50">
-      <h1 className="mt-10 font-bold text-5xl bg-yellow-50">Sandrine MANSON</h1>
-      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">
-        Développeuse web et web mobile React JS
-      </h2>
+      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Curriculum</h2>
       <p className="mt-5 vhitespace-norma bg-yellow-50l">
         Durant ma formation bootcamp à la Wild Code School j`ai travaillé sur
         divers projets : <br />
@@ -29,9 +26,6 @@ function Curiculum() {
           <li>développement de mon portfolio en React JS et TailwindCSS</li>
         </ul>
       </p>
-      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Mes projets</h2>
-      <img className="mt-10 w-36  bg-yellow-50" src={photo}></img>
-      <img className="mt-10 w-36  bg-yellow-50" src={photo2}></img>
     </div>
   );
 }
