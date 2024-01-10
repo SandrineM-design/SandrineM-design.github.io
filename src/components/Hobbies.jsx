a0c3b36755cba776cdd9c7c6a51ebf189fed8f62
SandrineM-design <mansonsandrine@gmail.com>
2024-01-10 16:22:47 +0100

    integrate hobbies component

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-function Hobbies(props) {
+function Hobbies() {
   return (
     <div>
+      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Mes passe-temps</h2>
       Sur mon temps libre, je suis Sapeure Pompier Volontaire au centre de
       secours de <br />
       ma commune de résidence. Mes missions principales sont le secours aux
-      personnes, la mise en sécurité <br />
+      personnes et la mise en sécurité <br />
       des biens et des personnes dans les lieux privés ou publics. <br />
-      J'aime les balades dans la nature, le bricolage, le jardinage, ... en
-      famille. Je suis également une grande lectrice. Mes styles favoris sont
-      les romans policiers, la science <br />
+      J'aime les balades dans la nature, le bricolage, le jardinage, ... seule
+      ou en famille. <br />
+      Je suis également une grande lectrice. Mes styles favoris sont les romans
+      policiers, la science <br />
       fiction et les romans historiques.
     </div>
   );
