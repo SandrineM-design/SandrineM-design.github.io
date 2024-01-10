f422cb4cab22508cd4d862d96478a59c9875e3d9
SandrineM-design <mansonsandrine@gmail.com>
2024-01-10 15:57:16 +0100

    add contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
-function Contact(props) {
-  return <div>Contact</div>;
+function Contact() {
+  return (
+    <div>
+      <h1></h1>
+      <form>
+        <label>
+          Nom :
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Prénom :
+          <input type="text" name="firstname" />
+        </label>
+        <label>
+          Votre message :
+          <input type="text" name="message" />
+        </label>
+        <input type="submit" value="Envoyer" />
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
