be1d00ba3727e0e6e55b5d0fe255940ed0374df4
SandrineM-design <mansonsandrine@gmail.com>
2024-01-10 11:43:07 +0100

    add new project on curriculum

--- src/pages/Curiculum.jsx
+++ src/pages/Curiculum.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import photo from "../assets/capture_ToDO_List.jpg";
+import photo2 from "../assets/Juracine.jpg";
 
-function Curiculum(props) {
+function Curiculum() {
   return (
     <div className="flex flex-col flex-1 place-items-center bg-yellow-50">
       <h1 className="mt-10 font-bold text-5xl bg-yellow-50">Sandrine MANSON</h1>
@@ -31,6 +31,7 @@ function Curiculum(props) {
       </p>
       <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Mes projets</h2>
       <img className="mt-10 w-36  bg-yellow-50" src={photo}></img>
+      <img className="mt-10 w-36  bg-yellow-50" src={photo2}></img>
     </div>
   );
 }
