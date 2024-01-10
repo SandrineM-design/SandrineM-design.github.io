6e6f55e146e8a952991afb97ba0adbf4d962a53f
SandrineM-design <mansonsandrine@gmail.com>
2024-01-10 16:38:48 +0100

    integrate skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,49 @@
-import React from "react";
-
-function Skills(props) {
-  return <div></div>;
+function Skills() {
+  return (
+    <div>
+      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Hard skills</h2>
+      <h3>Languages :</h3>
+      <ul>
+        <li>✅HTML 5</li>
+        <li>✅CSS 3</li>
+        <li>✅Javascript Vanilla</li>
+      </ul>
+      <h3>Frameworks & libraries :</h3>
+      <ul>
+        <li>✅React JS</li>
+        <li>✅Express JS</li>
+        <li>✅Node JS</li>
+        <li>✅Vite</li>
+        <li>✅VueJS</li>
+        <li>✅Sass</li>
+        <li>✅Tailwind CSS</li>
+        <li>✅NPM</li>
+        <li>✅JEST</li>
+        <li>✅Postman</li>
+      </ul>
+      <h3>Databases :</h3>
+      <ul>
+        <li>✅MySQL</li>
+        <li>✅SQLite</li>
+        <li>✅MongoDB</li>
+      </ul>
+      <h3> Design :</h3>
+      <ul>
+        <li>✅ Figma</li>
+        <li>✅Canva</li>
+      </ul>
+      <h3> Tools :</h3>
+      <ul>
+        <li>✅Git</li>
+        <li>✅GitHub</li>
+        <li>✅VSCode</li>
+        <li>✅Microsoft Office</li>
+        <li>✅EsLint</li>
+        <li>✅Prettier</li>
+        <li>✅Trello</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Skills;
