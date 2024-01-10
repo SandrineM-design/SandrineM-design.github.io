d5f5cefa0cd12d71f4e2e72458a614d5fd3b329a
SandrineM-design <mansonsandrine@gmail.com>
2024-01-10 17:15:34 +0100

    integrate contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,14 @@
+import linkedin from "../assets/Linkedin_icon.png";
+import github from "../assets/github.png";
+
 function Contact() {
   return (
     <div>
-      <h1></h1>
+      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">
+        Une question, une proposition d'emploi ou vous souhaitez simplement
+        faire connaissance ? Envoyez-moi un message ou contactez-moi sur les
+        réseaux !
+      </h2>
       <form>
         <label>
           Nom :
@@ -17,6 +24,20 @@ function Contact() {
         </label>
         <input type="submit" value="Envoyer" />
       </form>
+
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/sandrine-manson-040925290/">
+            <img src={linkedin}></img>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/SandrineM-design">
+            {" "}
+            <img src={github}></img>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
