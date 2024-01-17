cfc5795f5005aed7a359b8ad5c992c42248c5bc3
SandrineM-design <mansonsandrine@gmail.com>
2024-01-17 15:24:47 +0100

    create array for hard skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,47 +1,62 @@
 function Skills() {
+  const languages = [
+    {
+      name1: "HTML 5",
+      name2: "CSS3",
+      name3: "Javascript Vanilla",
+    },
+  ];
+
+  const framworksLibraries = [
+    {
+      name1: "React JS",
+      name2: "Express JS",
+      name3: "Node JS",
+      name4: "Vite",
+      name5: "VueJS",
+      name6: "Sass",
+      name7: "TailwindCSS",
+      name8: "NPM",
+      name9: "JEST",
+      name10: "Postman",
+    },
+  ];
+
+  const databases = [
+    {
+      name1: "MySQL",
+      name2: "SQLite",
+      name3: "MongoDB",
+    },
+  ];
+
+  const design = [
+    {
+      name1: "Figma",
+      name2: "Canva",
+    },
+  ];
+
+  const tools = [
+    {
+      name1: "Git",
+      name2: "GitHub",
+      name3: "VSCode",
+      name4: "Microsoft Office",
+      name5: "EsLint",
+      name6: "Prettier",
+      name7: "Trello",
+    },
+  ];
+
   return (
     <div>
       <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Hard skills</h2>
-      <h3>Languages :</h3>
-      <ul>
-        <li>✅HTML 5</li>
-        <li>✅CSS 3</li>
-        <li>✅Javascript Vanilla</li>
-      </ul>
-      <h3>Frameworks & libraries :</h3>
-      <ul>
-        <li>✅React JS</li>
-        <li>✅Express JS</li>
-        <li>✅Node JS</li>
-        <li>✅Vite</li>
-        <li>✅VueJS</li>
-        <li>✅Sass</li>
-        <li>✅Tailwind CSS</li>
-        <li>✅NPM</li>
-        <li>✅JEST</li>
-        <li>✅Postman</li>
-      </ul>
-      <h3>Databases :</h3>
-      <ul>
-        <li>✅MySQL</li>
-        <li>✅SQLite</li>
-        <li>✅MongoDB</li>
-      </ul>
-      <h3> Design :</h3>
-      <ul>
-        <li>✅ Figma</li>
-        <li>✅Canva</li>
-      </ul>
-      <h3> Tools :</h3>
-      <ul>
-        <li>✅Git</li>
-        <li>✅GitHub</li>
-        <li>✅VSCode</li>
-        <li>✅Microsoft Office</li>
-        <li>✅EsLint</li>
-        <li>✅Prettier</li>
-        <li>✅Trello</li>
-      </ul>
+      <h3>Langages de programmation :</h3>
+      <h3>Frameworks & bibliothèques :</h3>
+      <h3>Bases de données :</h3>
+      <h3>Design :</h3>
+      <h3>Outils :</h3>
     </div>
   );
 }
