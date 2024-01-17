b3c934474d814bd715c14e1cb6063608baecf62c
SandrineM-design <mansonsandrine@gmail.com>
2024-01-17 14:35:50 +0100

    create array of projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,64 +1,37 @@
-import photo from "../assets/capture_ToDO_List.jpg";
-import photo2 from "../assets/Juracine.jpg";
-import photo3 from "../assets/origins_digital.jpg";
+import ToDoList from "../assets/capture_ToDO_List.jpg";
+import Juracine from "../assets/Juracine.jpg";
+import Originsdigital from "../assets/origins_digital.jpg";
 
 function Projects() {
+  const projects = [
+    {
+      name: "ToDo List",
+      image: { ToDoList },
+      description: "Technos utilisées : Javascript, CSS",
+      link: "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P1-4Fantascripts",
+      repository:
+        "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P1-4Fantascripts",
+    },
+    {
+      name: "Juraciné",
+      image: { Juracine },
+      description: "Technos utilisées : React JS et Sass",
+      link: "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P2-Jurastream",
+      repository:
+        "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P2-Jurastream",
+    },
+    {
+      name: "Origin's Digital",
+      image: {Originsdigital},
+      description : "Technos utilisées: NodeJS, MySQL, ReactJS, Sass",
+      link: "https://origins.remote-6.wilders.dev/",
+      repository: "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P3-Originsdigital" 
+    }
+  ];
   return (
-    <div>
-      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Mes projets</h2>
-      <h3 className="mt-10 w-36 bg-yellow-50">
-        <img className="mt-10 w-36  bg-yellow-50" src={photo}></img>
-        ToDo list en Javascript et CSS
-      </h3>
-      <ul>
-        <li>
-          <a href="https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P1-4Fantascripts">
-            {" "}
-            Lien du repo GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://wildcodeschool-2023-09.github.io/JS-RemoteFR-jurascripts-P1-4Fantascripts/">
-            Lien vers le site web
-          </a>
-        </li>
-      </ul>
-
-      <h3 className="mt-10 w-36 bg-yellow-50">
-        <img className="mt-10 w-36  bg-yellow-50" src={photo2}></img> Juraciné :
-        clone du site Allo Ciné en React JS et Sass
-      </h3>
-      <ul>
-        <li>
-          <a href="https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P2-Jurastream">
-            {" "}
-            Lien du repo GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://jurastream.vercel.app/">Lien vers le site web</a>
-        </li>
-      </ul>
-      <h3 className="mt-10 w-36 bg-yellow-50">
-        <img className="mt-10 w-36  bg-yellow-50" src={photo3}></img> Origin's
-        Digital : Site style CMS en ReactJS, Sass avec base en données en
-        méthode MERISE
-      </h3>
-      <ul>
-        <li>
-          <a href="https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P3-Originsdigital">
-            {" "}
-            Lien du repo GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://origins.remote-6.wilders.dev/">
-            Lien vers le site web
-          </a>
-        </li>
-      </ul>
-    </div>
+    
   );
-}
+    
+  
 
 export default Projects;
