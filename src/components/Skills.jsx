b74c93df253ed73e52e039caeb244ae1e067c7e8
SandrineM-design <mansonsandrine@gmail.com>
2024-02-01 16:56:23 +0100

    css on skills file

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,34 +29,40 @@ function Skills() {
   ];
 
   return (
-    <div>
+    <div className="flex flex-col flex-1 place-items-center bg-yellow-50">
       <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Hard skills</h2>
-      <h3>Langages de programmation :</h3>
-      <ul>
+      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">
+        Langages de programmation :
+      </h3>
+      <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {languages.map((language, index) => (
           <li key={index}>{language}</li>
         ))}
       </ul>
-      <h3>Frameworks & bibliothèques :</h3>
-      <ul>
+      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">
+        Frameworks & bibliothèques :
+      </h3>
+      <ul className="flex flex-col-1 gap-10 mt-2 mb-2 list-disc">
         {framworksLibraries.map((framworksLib, index) => (
           <li key={index}>{framworksLib}</li>
         ))}
       </ul>
-      <h3>Bases de données :</h3>
-      <ul>
+      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">
+        Bases de données :
+      </h3>
+      <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {databases.map((database, index) => (
           <li key={index}>{database}</li>
         ))}
       </ul>
-      <h3>Design :</h3>
-      <ul>
+      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">Design :</h3>
+      <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {design.map((design, index) => (
           <li key={index}>{design}</li>
         ))}
       </ul>
-      <h3>Outils :</h3>
-      <ul>
+      <h3 className="mt-2.5 font-bold text-1x1  bg-yellow-50">Outils :</h3>
+      <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {tools.map((tool, index) => (
           <li key={index}>{tool}</li>
         ))}
