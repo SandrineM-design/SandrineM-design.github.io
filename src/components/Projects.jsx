13f77c521c788c1c0868670b2dee6bd944d212fa
SandrineM-design <mansonsandrine@gmail.com>
2024-02-01 10:29:37 +0100

    code review

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
   const projects = [
     {
       name: "ToDo List",
-      image: { ToDoList },
+      image: ToDoList,
       description: "Technos utilisées : Javascript, CSS",
       link: "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P1-4Fantascripts",
       repository:
@@ -14,7 +14,7 @@ function Projects() {
     },
     {
       name: "Juraciné",
-      image: { Juracine },
+      image: Juracine,
       description: "Technos utilisées : React JS et Sass",
       link: "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P2-Jurastream",
       repository:
@@ -22,16 +22,27 @@ function Projects() {
     },
     {
       name: "Origin's Digital",
-      image: {Originsdigital},
-      description : "Technos utilisées: NodeJS, MySQL, ReactJS, Sass",
+      image: Originsdigital,
+      description: "Technos utilisées: NodeJS, MySQL, ReactJS, Sass",
       link: "https://origins.remote-6.wilders.dev/",
-      repository: "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P3-Originsdigital" 
-    }
+      repository:
+        "https://github.com/WildCodeSchool-2023-09/JS-RemoteFR-jurascripts-P3-Originsdigital",
+    },
   ];
+
   return (
-    
+    <div>
+      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">Mes projets</h2>
+      <ul>
+        {projects.map((project, index) => (
+          <li key={index}>
+            {project.name} {project.image} {project.description} {project.link}{" "}
+            {project.repository}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-    
-  
+}
 
 export default Projects;
