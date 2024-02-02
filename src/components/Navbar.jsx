ad3bb1927c65d647aeacd1e8c382e61ba41a81d4
SandrineM-design <mansonsandrine@gmail.com>
2024-02-02 11:15:29 +0100

    update navbar with a logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,44 @@
+import logo from "../assets/logo.png";
+
 function Navbar() {
   return (
     <ul className="flex">
-      <li className="mr-6">
-        <a className="font-bold text-green-600 hover:text-green-800" href="/">
-          Présentation
+      <img className="w-20 h-auto rounded-full " src={logo}></img>
+      <li className="mr-6 mt-5">
+        <a
+          className="font-bold text-emerald-600 hover:text-emerald-800"
+          href="/"
+        >
+          Accueil
         </a>
       </li>
-      <li className="mr-6">
+      <li className="mr-6 mt-5">
         <a
-          className="font-bold text-green-600 hover:text-green-800"
+          className="font-bold text-emerald-600 hover:text-emerald-800"
+          href="/skills"
+        >
+          Compétences
+        </a>
+      </li>
+      <li className="mr-6 mt-5">
+        <a
+          className="font-bold text-emerald-600 hover:text-emerald-800"
           href="/curiculum"
         >
-          Curiculum Vitae
+          Expériences
         </a>
       </li>
-      <li className="mr-6">
+      <li className="mr-6 mt-5">
         <a
-          className="font-bold text-green-600 hover:text-green-800"
+          className="font-bold text-emerald-600 hover:text-emerald-800"
           href="/projects"
         >
           Projets
         </a>
       </li>
-      <li className="mr-6">
+      <li className="mr-6 mt-5">
         <a
-          className="font-bold text-green-600 hover:text-green-800"
+          className="font-bold text-emerald-600 hover:text-emerald-800"
           href="/contact"
         >
           Contact
