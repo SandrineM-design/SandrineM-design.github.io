d0c986b6d24601d863c8e5b03142612e8b3f2c3c
SandrineM-design <mansonsandrine@gmail.com>
2024-02-02 11:15:54 +0100

    update presentation file

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,35 +1,36 @@
 import photo from "../assets/Sandrine.jpg";
-import Curiculum from "../pages/Curiculum";
-import Projects from "./Projects";
-import Hobbies from "./Hobbies";
-import Skills from "./Skills";
-import Contact from "../pages/Contact";
-import Education from "./Education";
 
 function Presentation() {
   return (
-    <div className="flex flex-col flex-1 place-items-center bg-yellow-50">
-      <h1 className="mt-10 font-bold text-5xl bg-yellow-50">
-        {" "}
+    <div className="flex flex-col flex-1 place-items-center">
+      <h1 className="mt-20 font-bold text-5xl text-emerald-600 ">
         Sandrine MANSON
       </h1>
-      <img
-        className="mt-10 w-36 h-auto rounded-full bg-yellow-50"
-        src={photo}
-      ></img>
-      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">
-        Développeuse web et web mobile
+      <img className="mt-10 w-36 h-auto rounded-full" src={photo}></img>
+      <h2 className="mt-10 font-bold text-2xl text-emerald-600">
+        Développeuse web et web mobile junior
       </h2>
-      <p className="mt-5 vhitespace-norma bg-yellow-50l">
-        Bienvenue sur mon portfolio, fenêtre ouverte sur mes projets dev. <br />
-        Vous y trouverez tous mes projets, personnels ou de formation. <br />
+      <p className="mt-5 mb-5 vhitespace-normal text-center ">
+        Après 20 ans à travailler auprès des enfants et des familles, <br />
+        j'ai décidé de changer radicalement de voie et de me reconvertir <br />
+        dans le développement informatique.
       </p>
-      <Education />
-      <Curiculum />
-      <Projects />
-      <Skills />
-      <Hobbies />
-      <Contact />
+      <p className="mt-5 mb-5 vhitespace-normal text-center ">
+        En tant que développeuse web junior, j'ai à coeur de continuer à
+        progresser <br />
+        et d'approndir mes connaissances afin de rapidement monter en
+        compétences.
+      </p>
+      <p className="mt-5 mb-5 vhitespace-normal text-center ">
+        Autodidacte au début de mon apprentissage du développement web, j'ai
+        effectué une formation <br />
+        avec la Wild Code School afin d'obtenir mon diplome de Développeuse Web
+        et Web Mobile.
+      </p>
+      <p className="mt-5 mb-20 vhitespace-normal text-center ">
+        Vous trouverez ici quelques uns de mes projets, miroirs de mes
+        compétences et savoir-faire.
+      </p>
     </div>
   );
 }
