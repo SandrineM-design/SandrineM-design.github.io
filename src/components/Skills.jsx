21bae227b83d73eb174bd80abc46687da1ad2c3f
SandrineM-design <mansonsandrine@gmail.com>
2024-02-02 14:53:03 +0100

    update skills file

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,23 +13,23 @@ function Skills() {
   const tools = ["GitHub", "Git", "Figma"];
 
   return (
-    <div className="flex flex-col flex-1 place-items-center bg-yellow-50">
-      <h2 className="mt-10 font-bold text-2xl bg-yellow-50">
+    <div className="flex flex-col flex-1 place-items-center">
+      <h2 className="mt-20 font-bold text-2xl text-emerald-600">
         Compétences techniques
       </h2>
-      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">Frontend :</h3>
+      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-600">Frontend :</h3>
       <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {frontend.map((frontend, index) => (
           <li key={index}>{frontend}</li>
         ))}
       </ul>
-      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">Backend :</h3>
+      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-600">Backend :</h3>
       <ul className="flex flex-col-1 gap-10 mt-2 mb-2 list-disc">
         {backend.map((backend, index) => (
           <li key={index}>{backend}</li>
         ))}
       </ul>
-      <h3 className="mt-2.5 font-bold text-1x1 bg-yellow-50">Outils :</h3>
+      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-600">Outils :</h3>
       <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {tools.map((tool, index) => (
           <li key={index}>{tool}</li>
