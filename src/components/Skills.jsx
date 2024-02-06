ef516dcb619a88444ff7042cbeabfdfb1b00e2f7
SandrineM-design <mansonsandrine@gmail.com>
2024-02-06 10:02:45 +0100

    update skills file

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,19 +17,19 @@ function Skills() {
       <h2 className="mt-20 font-bold text-2xl text-emerald-600">
         Compétences techniques
       </h2>
-      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-600">Frontend :</h3>
+      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-400">Frontend :</h3>
       <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {frontend.map((frontend, index) => (
           <li key={index}>{frontend}</li>
         ))}
       </ul>
-      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-600">Backend :</h3>
+      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-400">Backend :</h3>
       <ul className="flex flex-col-1 gap-10 mt-2 mb-2 list-disc">
         {backend.map((backend, index) => (
           <li key={index}>{backend}</li>
         ))}
       </ul>
-      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-600">Outils :</h3>
+      <h3 className="mt-2.5 font-bold text-1x1 text-emerald-400">Outils :</h3>
       <ul className="flex flex-col-3 gap-10 mt-2 mb-2 list-disc">
         {tools.map((tool, index) => (
           <li key={index}>{tool}</li>
